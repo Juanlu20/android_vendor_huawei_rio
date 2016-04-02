0bd56aecd2eb0275c526450183b2cb6b6ad2ddf1
surdupetru <surdu_petru@yahoo.com>
2016-04-02 23:46:27 +0200

    add RIO blobs

--- proprietary/sbin/chargelog.sh
+++ proprietary/sbin/chargelog.sh
@@ -123,14 +123,12 @@ file_size=0
 
 #---------------------------------------------------------------------------------------------------------------------------------
 # get log type by product board id
-#/* <DTS2015082203230 yinfenghang 20150822 begin */
 
 log_type_qcom=0
 log_type_ti=1
 log_type_maxim=2
 log_type_ti_low=3
 log_type_ti_only=4
-log_type_ti_bms=5
 BOARD_ID_TI=( \
 "MSM8939_ATH" \
 "MSM8939_RIO" \
@@ -148,9 +146,6 @@ BOARD_ID_TI_LOW=( \
 BOARD_ID_TI_ONLY=( \
 "MSM8939_CHM-CL00_VB" \
 "MSM8939_C8818" \
-)
-BOARD_ID_TI_BMS=( \
-"MSM8939_KIW_TL00H_VT" \
 )
 
 log_type=$log_type_qcom
@@ -188,15 +183,6 @@ do
         break
     fi
 done
-for board_id in ${BOARD_ID_TI_BMS[@]}
-do
-    cat /proc/app_info | grep "$board_id"
-    if [ $? -eq 0 ];then
-        log_type=$log_type_ti_bms
-        break
-    fi
-done
-#/* DTS2015082203230 yinfenghang 20150822 end> */
 #---------------------------------------------------------------------------------------------------------------------------------
 
 if [ $log_type -eq $log_type_maxim ];then
@@ -400,159 +386,6 @@ elif [ $log_type -eq $log_type_ti ];then
         sleep $1
     done
 #Ti log end
-#/* <DTS2015082203230 yinfenghang 20150822 begin */
-elif [ $log_type -eq $log_type_ti_bms ];then
-#---------------------------------------------------------------------------------------------------------------------------------
-#Ti BMS log begin
-    echo -n "Date Time,     Voltage Current SOC RM FCC II SI Temp FLAG Status Qmax ufrm frm uffcc ffcc ufsoc Voltage brightness SOC TEMP,Reg[0],Reg[1],Reg[2],Reg[3],Reg[4],Reg[5],Reg[6],Reg[7],Reg[8],Reg[9],Reg[0A],ibus,usbonline,usbscope,usbtype,usbcurrentmax,batterypresent,hot_current_limit,health,status,charging_enabled,<cpu0_1_2_3><cpu4_5_6_7>,cpu0_cpuinfo_cur_freq,cpu0_scaling_max_freq,cpu4_cpuinfo_cur_freq,cpu4_scaling_max_freq,tsens_tz_sensor0,tsens_tz_sensor1,tsens_tz_sensor2[pop_mem],tsens_tz_sensor3[gpu],tsens_tz_sensor5[cpu0],tsens_tz_sensor6[cpu1],tsens_tz_sensor7[cpu2],tsens_tz_sensor8[cpu3],tsens_tz_sensor9[cpu4567],beside_cpu_tsens,beside_camera_tsens,Mode" >> $log_path
-    echo >> $log_path
-    chmod 755 /data/log/chargelog/chargelog.csv
-    while true
-    do
-        echo -n `date +%0m-%0e` >> $log_path
-        echo -n ' ' >> $log_path
-        echo -n `date +%H:%M:%S` >> $log_path
-        echo -n , >> $log_path
-
-		echo -n `cat /sys/bus/i2c/drivers/ti,bq27510/ti-bms-prop/gaugelog` >> $log_path
-        echo -n ' ' >> $log_path
-        echo -n `cat /sys/class/power_supply/battery/voltage_now` >> $log_path
-        echo -n ' ' >> $log_path
-        echo -n `cat /sys/class/leds/lcd-backlight/brightness` >> $log_path
-        echo -n ' ' >> $log_path
-
-        echo -n `cat /sys/class/power_supply/battery/capacity` >> $log_path
-        echo -n ' ' >> $log_path
-        echo -n `cat /sys/class/power_supply/battery/temp` >> $log_path
-        echo -n , >> $log_path
-        echo -n `cat /sys/class/power_supply/battery/device/chargelog` >> $log_path
-        #echo -n , >> $log_path
-
-        echo -n `cat /sys/class/hw_power/charger/charge_data/ibus` >> $log_path
-        echo -n , >> $log_path
-        echo -n `cat /sys/class/power_supply/usb/online` >> $log_path
-        echo -n , >> $log_path
-        echo -n `cat /sys/class/power_supply/usb/scope` >> $log_path
-        echo -n , >> $log_path
-        echo -n `cat /sys/class/power_supply/usb/type` >> $log_path
-        echo -n , >> $log_path
-        echo -n `cat /sys/class/power_supply/usb/current_max` >> $log_path
-        echo -n , >> $log_path
-        echo -n `cat /sys/class/power_supply/battery/present` >> $log_path
-        echo -n , >> $log_path
-        echo -n `cat /sys/class/power_supply/battery/hot_current_limit` >> $log_path
-        echo -n , >> $log_path
-
-        echo -n `cat /sys/class/power_supply/battery/health` >> $log_path
-        echo -n , >> $log_path
-        echo -n `cat /sys/class/power_supply/battery/status` >> $log_path
-        echo -n , >> $log_path
-        echo -n `cat /sys/class/power_supply/battery/charging_enabled` >> $log_path
-        echo -n , >> $log_path
-
-        echo -n '   <' >> $log_path
-        echo -n `cat /sys/devices/system/cpu/cpu0/online`     >> $log_path
-        echo -n '_' >> $log_path
-        echo -n `cat /sys/devices/system/cpu/cpu1/online`     >> $log_path
-        echo -n '_' >> $log_path
-        echo -n `cat /sys/devices/system/cpu/cpu2/online`     >> $log_path
-        echo -n '_' >> $log_path
-        echo -n `cat /sys/devices/system/cpu/cpu3/online`     >> $log_path
-
-        echo -n '>   <' >> $log_path
-        echo -n `cat /sys/devices/system/cpu/cpu4/online`     >> $log_path
-        echo -n '_' >> $log_path
-        echo -n `cat /sys/devices/system/cpu/cpu5/online`     >> $log_path
-        echo -n '_' >> $log_path
-        echo -n `cat /sys/devices/system/cpu/cpu6/online`     >> $log_path
-        echo -n '_' >> $log_path
-        echo -n `cat /sys/devices/system/cpu/cpu7/online`     >> $log_path
-        echo -n '>' >> $log_path
-        echo -n , >> $log_path
-
-        for i in 0 1 2 3
-        do
-            online=`cat /sys/devices/system/cpu/cpu$i/online`
-            if [[ $online = 1 ]] ;then
-                echo -n `cat /sys/devices/system/cpu/cpu$i/cpufreq/cpuinfo_cur_freq` >> $log_path
-                echo -n , >> $log_path
-                echo -n `cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_max_freq` >> $log_path
-                echo -n , >> $log_path
-                break;
-            fi
-
-            if [[ $i = 3 ]] ;then
-                echo -n ' '  >> $log_path
-                echo -n , >> $log_path
-                echo -n ' '  >> $log_path
-                echo -n , >> $log_path
-            fi
-        done;
-
-
-        for i in 4 5 6 7
-        do
-            online=`cat /sys/devices/system/cpu/cpu$i/online`
-            if [[ $online = 1 ]] ;then
-                echo -n `cat /sys/devices/system/cpu/cpu$i/cpufreq/cpuinfo_cur_freq` >> $log_path
-                echo -n , >> $log_path
-                echo -n `cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_max_freq` >> $log_path
-                echo -n , >> $log_path
-                break;
-            fi
-            if [[ $i = 7 ]] ;then
-                echo -n ' '  >> $log_path
-                echo -n , >> $log_path
-                echo -n ' '  >> $log_path
-                echo -n , >> $log_path
-            fi
-        done;
-
-        echo -n `cat /sys/devices/virtual/thermal/thermal_zone0/temp` >> $log_path
-        echo -n , >> $log_path
-        echo -n `cat /sys/devices/virtual/thermal/thermal_zone1/temp` >> $log_path
-        echo -n , >> $log_path
-        echo -n `cat /sys/devices/virtual/thermal/thermal_zone2/temp` >> $log_path
-        echo -n , >> $log_path
-        echo -n `cat /sys/devices/virtual/thermal/thermal_zone3/temp` >> $log_path
-        echo -n , >> $log_path
-        echo -n `cat /sys/devices/virtual/thermal/thermal_zone4/temp` >> $log_path
-        echo -n , >> $log_path
-        echo -n `cat /sys/devices/virtual/thermal/thermal_zone5/temp` >> $log_path
-        echo -n , >> $log_path
-        echo -n `cat /sys/devices/virtual/thermal/thermal_zone6/temp` >> $log_path
-        echo -n , >> $log_path
-        echo -n `cat /sys/devices/virtual/thermal/thermal_zone7/temp` >> $log_path
-        echo -n , >> $log_path
-		echo -n `cat /sys/devices/virtual/thermal/thermal_zone8/temp` >> $log_path
-        echo -n , >> $log_path
-        echo -n `cat /sys/module/qpnp_adc_voltage/parameters/therm_cpu` >> $log_path
-        echo -n , >> $log_path
-        echo -n `cat /sys/module/qpnp_adc_voltage/parameters/therm_cam` >> $log_path
-        echo -n , >> $log_path
-
-        if [ charge_mode -eq 1 ];then
-            echo -n "ON" >> $log_path
-        else
-            echo -n "OFF" >> $log_path
-        fi
-
-        echo >> $log_path
-
-        #get log file size
-        TEMP=`ls -s $log_path`
-        arr=(${TEMP})
-        file_size=${arr[0]}
-
-        if [ $file_size -ge $2 ];then
-            mv $log_path $log_path".old.csv"
-            file_size=0
-        fi
-        sync
-        sleep $1
-    done
-#Ti log end
-#/* DTS2015082203230 yinfenghang 20150822 end> */
 #---------------------------------------------------------------------------------------------------------------------------------
 elif [ $log_type -eq $log_type_ti_low ];then
 #---------------------------------------------------------------------------------------------------------------------------------
